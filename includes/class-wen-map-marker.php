af76875da9044cb918e920366279f4ebd053b2d3
Nilambar Sharma <nilambar@outlook.com>
2015-04-24 14:08:40 +0545

    to tag 1.1

--- includes/class-wen-map-marker.php
+++ includes/class-wen-map-marker.php
@@ -69,7 +69,7 @@ class WEN_Map_Marker {
 	public function __construct() {
 
 		$this->wen_map_marker = 'wen-map-marker';
-		$this->version = '1.0.0';
+		$this->version = '1.1';
 
 		$this->load_dependencies();
 		$this->set_locale();
